81ee9bede59942a1422dae3643a1bf655137c1bd
Edward Agarwala <edwardagarwala@gmail.com>
2025-01-20 19:37:38 +0000

    Include hyperopt parameter tuning.

--- src/build_gbm_model.py
+++ src/build_gbm_model.py
@@ -3,29 +3,41 @@ from pathlib import Path
 
 import pandas as pd
 import xgboost as xgb
-from numpy import ndarray
+from numpy import ndarray, arange, argmin
 from sklearn.metrics import mean_squared_error
+from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
 
 from src.compare_predictions import RegressionMetrics, evaluate_regression
 from src.split_data import TRAIN_DATA_PATH, TUNE_DATA_PATH
 from src.xgb_prepare_data import prepare_data
 
 TARGET_COLUMN = "Y"
-FEATURE_COLUMNS = ["AGE", "SEX_NUM", "BMI", "BP", "S1", "S2", "S3", "S4", "S5", "S6"]
+FEATURE_COLUMNS: list[str] = [
+    "AGE",
+    "SEX_NUM",
+    "BMI",
+    "BP",
+    "S1",
+    "S2",
+    "S3",
+    "S4",
+    "S5",
+    "S6",
+]
 WEIGHT_COLUMN = None
 RANDOM_SEED: int = 422134987614
 MODEL_PARAMS: dict = {
     "objective": "reg:squarederror",
     "device": "gpu",
     "tree_method": "hist",
-    "max_depth": 6,
+    "max_depth": hp.choice("max_depth", arange(2, 10, 1)),
     "learning_rate": 0.1,
-    "max_bin": 31,
-    "colsample_bytree": 1,
-    "colsample_bylevel": 1,
-    "subsample": .8,
+    "max_bin": hp.choice("max_bin", arange(2, 256, 1)),
+    "colsample_bytree": hp.uniform("colsample_bytree", 0.8, 1),
+    "colsample_bylevel": hp.uniform("colsample_bylevel", 0.8, 1),
+    "subsample": hp.uniform("subsample", 0.5, 1),
     "gamma": 0,
-    "min_child_weight": 1,
+    "min_child_weight": hp.choice("min_child_weight", arange(1, 10, 1)),
     "seed": RANDOM_SEED,
     "num_boost_round": 100,
     "early_stopping_rounds": 10,
@@ -33,6 +45,14 @@ MODEL_PARAMS: dict = {
 
 XGB_MODEL_FILENAME = "data/xgboost_model/model.ubj"
 
+
+def getBestModelfromTrials(trials):
+    valid_trial_list = [
+        trial for trial in trials if STATUS_OK == trial["result"]["status"]
+    ]
+    losses = [float(trial["result"]["loss"]) for trial in valid_trial_list]
+    index_having_minumum_loss:int = argmin(losses)
+    return valid_trial_list[index_having_minumum_loss]["result"]["model"]
 
 def score_model(model: xgb.Booster, data: xgb.DMatrix) -> RegressionMetrics:
 
@@ -51,13 +71,32 @@ def build_xgboost_model(
     params: dict = MODEL_PARAMS,
 ):
 
-    model: xgb.Booster = xgb.train(
-        params,
-        train_data,
-        evals=[(train_data, "train"), (tune_data, "tune")],
-        num_boost_round=params["num_boost_round"],
-        early_stopping_rounds=params["early_stopping_rounds"],
+    trials = Trials()
+
+    def objective(params: dict) -> dict:
+        model: xgb.Booster = xgb.train(
+            params,
+            train_data,
+            evals=[(train_data, "train"), (tune_data, "tune")],
+            num_boost_round=params["num_boost_round"],
+            early_stopping_rounds=params["early_stopping_rounds"],
+        )
+
+        tune_results = score_model(model, tune_data)
+
+        return {"loss": tune_results.rmse, "status": STATUS_OK, "model": model}
+
+    best_params = fmin(
+        fn=objective,
+        space=params,
+        algo=tpe.suggest,
+        max_evals=100,
+        trials=trials,
     )
+
+    print(f"Best Parameters: {best_params}")
+
+    model = getBestModelfromTrials(trials)
 
     # Score the model
     train_results = score_model(model, train_data)
@@ -66,7 +105,7 @@ def build_xgboost_model(
     print(f"Tuning Results: {tune_results}")
 
     # Save the model
-    model_path = Path(XGB_MODEL_FILENAME).parent
+    model_path: Path = Path(XGB_MODEL_FILENAME).parent
     model_path.mkdir(parents=True, exist_ok=True)
     model.save_model(XGB_MODEL_FILENAME)
     print(f"Model saved to {model_path}")
